910a9602de53cce14c327391ec58d2aeb424454f
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-11 23:10:29 +0100

    [Feature #160359249] Write test for API endpoint that place orders

--- dummyServer/test/order.spec.js
+++ dummyServer/test/order.spec.js
@@ -1,6 +1,7 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app';
+import mockOrders from './mockData/order';
 
 chai.use(chaiHttp);
 chai.should();
@@ -102,6 +103,111 @@ describe('API endpoint GET /orders/:id', () => {
         res.body.should.have.property('error');
         res.body.error.should.have.property('message');
         expect(res.body.error.message).to.equal('No Order matches the ID of 50203');
+        done();
+      });
+  });
+});
+
+describe('API endpoint POST /orders/', () => {
+  it('it should successfully place an order', (done) => {
+    chai.request(app)
+      .post('/api/v1/orders/')
+      .send(mockOrders[0])
+      .end((err, res) => {
+        res.should.have.status(201);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('message');
+        res.body.should.have.property('order');
+        expect(res.body.order).to.be.an('object');
+        res.body.order.should.have.keys(
+          'id', 'foodId',
+          'customerId', 'quantity', 'createdAt',
+          'updatedAt', 'deliveryAddress',
+          'status', 'phoneNumber'
+        );
+        expect(res.body.order.status).to.equal('pending');
+        expect(res.body.message).to.equal('Order placed successfully');
+        expect(res.body.status).to.equal(201);
+        expect(res.body.success).to.equal(true);
+        done();
+      });
+  });
+  it('it should return 400 Bad request if empty data is submitted', (done) => {
+    chai.request(app)
+      .post('/api/v1/orders/')
+      .send(mockOrders[2])
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('error');
+        expect(res.body.error).to.be.an('object');
+        res.body.error.should.have.keys(
+          'foodId', 'customerId', 'phoneNumber',
+          'deliveryAddress', 'quantity'
+        );
+        expect(res.body.error.foodId)
+          .to.equal('Food ID is required, must be an integer');
+        expect(res.body.error.quantity)
+          .to.equal('Quantity of food to order is required, must be an integer');
+        expect(res.body.error.customerId)
+          .to.equal('Customer ID is required, must be an integer');
+        expect(res.body.error.deliveryAddress)
+          .to.equal('Delivery address is required, must not be less than 3 characters');
+        expect(res.body.error.phoneNumber)
+          .to.equal('Phone Number is required and must be of 11 characters');
+        expect(res.body.status).to.equal(400);
+        expect(res.body.success).to.equal(false);
+        done();
+      });
+  });
+  it('it should return 400 Bad request', (done) => {
+    chai.request(app)
+      .post('/api/v1/orders/')
+      .send(mockOrders[3])
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('error');
+        expect(res.body.error).to.be.an('object');
+        res.body.error.should.have.keys(
+          'foodId', 'customerId', 'phoneNumber',
+          'deliveryAddress', 'quantity'
+        );
+        expect(res.body.error.foodId)
+          .to.equal('Food ID is required, must be an integer');
+        expect(res.body.error.quantity)
+          .to.equal('Quantity of food to order is required, must be an integer');
+        expect(res.body.error.customerId)
+          .to.equal('Customer ID is required, must be an integer');
+        expect(res.body.error.deliveryAddress)
+          .to.equal('Delivery address is required, must not be less than 3 characters');
+        expect(res.body.error.phoneNumber)
+          .to.equal('Phone Number is required and must be of 11 characters');
+        expect(res.body.status).to.equal(400);
+        expect(res.body.success).to.equal(false);
+        done();
+      });
+  });
+  it('it should return 404 Not Found if User and Food doesn\'t exist', (done) => {
+    chai.request(app)
+      .post('/api/v1/orders/')
+      .send(mockOrders[1])
+      .end((err, res) => {
+        res.should.have.status(404);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('error');
+        expect(res.body.error).to.be.an('object');
+        expect(res.body.status).to.equal(404);
+        expect(res.body.success).to.equal(false);
+        res.body.error.should.have.keys('foodId', 'customerId');
+        expect(res.body.error.foodId)
+          .to.equal(
+            `Order can't be placed because Food with ID ${mockOrders[1].foodId} doesn't exist`
+          );
+        expect(res.body.error.customerId)
+          .to.equal(
+            `Order can't be placed because User with ID ${mockOrders[1].customerId} doesn't exist`
+          );
         done();
       });
   });
