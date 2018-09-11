9e9f9d6012bae001b691536a48fd50bc58214ef5
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-11 16:54:52 +0100

    [Chore #160404417] Setup Data Structure to hold dummy data

---
+++ dummyServer/model/users.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    id: 1,
+    fullname: 'John Kepler',
+    email: 'johnkepler@mail.com',
+    password: '$2b$10$P2IIXdfVGN/W9Hqcr6GOrOQ3R68twx.kkXlBBKl4IFS8XvsPKvNjq',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+  {
+    id: 2,
+    fullname: 'Farayola Timileyin',
+    email: 'fartim96@mail.com',
+    password: '$2b$10$P2IIXdfVGN/W9Hqcr6GOrOQ3R68twx.kkXlBBKl4IFS8XvsPKvNjq',
+    isAdmin: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+];
+
+export default users;
