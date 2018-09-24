dc4d3bb9e7b40dd6c0aac465ef90189dd29e58b4
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-09-24 15:47:05 +0100

    [Finishes #160620147] Create API endpoint for signing in users

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -124,3 +124,90 @@ describe('API endpoint POST /auth/signup', () => {
       });
   });
 });
+
+
+describe('API endpoint POST /auth/login', () => {
+  it('it should successfully sign in a user', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'admin@foodfast.com',
+        password: 'farayola'
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('token');
+        expect(res.body.user).to.be.an('object');
+        expect(res.body.token).to.be.an('string');
+        expect(res.body.user.email).to.equal('admin@foodfast.com');
+        expect(res.body.user.fullname).to.equal('Food Fast');
+        expect(res.body.user.userId).to.equal('2a606085-5a9d-4481-a7e1-e7e98bea2160');
+        expect(res.body.user.isAdmin).to.equal(true);
+        res.body.should.have.property('message');
+        expect(res.body.message).to.equal('User sign-in was successful');
+        expect(res.body.status).to.equal(200);
+        expect(res.body.success).to.equal(true);
+        res.body.user.should.have.keys(
+          'userId', 'fullname', 'isAdmin', 'email'
+        );
+        done();
+      });
+  });
+  it('it should show 400 Bad Request if SignIn data is empty', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({})
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.success).to.equal(false);
+        expect(res.body.status).to.equal(400);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('error');
+        expect(res.body.error).to.be.an('object');
+        res.body.error.should.have.property('password');
+        res.body.error.should.have.property('email');
+        expect(res.body.error.password).to.equal('Password is required, must be between 8-20 characters');
+        expect(res.body.error.email).to.equal('Email is required, and must be a valid email');
+        done();
+      });
+  });
+  it('it should return 404 Not Found if User does not exist', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'faray@tmail.com',
+        password: 'hellotimmy'
+      })
+      .end((err, res) => {
+        res.should.have.status(404);
+        expect(res.body.success).to.equal(false);
+        expect(res.body.status).to.equal(404);
+        res.body.should.have.property('error');
+        expect(res.body).to.be.an('object');
+        expect(res.body.error).to.be.an('object');
+        res.body.error.should.have.property('message');
+        expect(res.body.error.message).to.equal('Login failed! User faray@tmail.com does not exist on the app');
+        done();
+      });
+  });
+  it('it should return 400 Bad Request if password is not correct', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'rafmme@yah.com',
+        password: 'inMyFeelings'
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.success).to.equal(false);
+        expect(res.body.status).to.equal(400);
+        res.body.should.have.property('error');
+        expect(res.body).to.be.an('object');
+        expect(res.body.error).to.be.an('object');
+        res.body.error.should.have.property('message');
+        expect(res.body.error.message).to.equal('Login failed! Password is not correct');
+        done();
+      });
+  });
+});
