bb5677d33e998f80d080f58fcf91d9dbd86c4847
Farayola Timileyin E <rafmme@gmail.com>
2018-09-30 22:47:46 +0000

    chore(review-feedback): Implement UI review feedback
    [Delivers #160544879]

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,61 +1,68 @@
 const editMealModal = document.getElementById('edit-meal');
-const updateOrderModal = document.getElementById('update')
-const addMealModal = document.getElementById('add-meal')
+const updateOrderModal = document.getElementById('update');
+const addMealModal = document.getElementById('add-meal');
 
 const toggleBtn = () => {
-    const x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+};
 
 const goToOrderpage = () => {
-    window.location.href = 'makeorder.html';
-}
+  window.location.href = 'makeorder.html';
+};
 
 const closeModalForm = (elemID) => {
-    const modal = document.getElementById(elemID);
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+  const modal = document.getElementById(elemID);
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
     }
-}
+  };
+};
 
 const closeModal = (elemID) => {
-    document.getElementById(elemID).style.display = 'none';
-}
+  document.getElementById(elemID).style.display = 'none';
+};
 
 const showModal = (elemID) => {
-    document.getElementById(elemID).style.display = 'block';
-    closeModalForm(elemID);
-}
+  document.getElementById(elemID).style.display = 'block';
+  closeModalForm(elemID);
+};
 
-const close = document.getElementsByClassName("closebtn");
+const close = document.getElementsByClassName('closebtn');
 let i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = () => {
-        const div = this.parentElement;
-        div.style.opacity = "0";
-        setTimeout(() => { div.style.display = "none"; }, 600);
-    }
+for (i = 0; i < close.length; i += 1) {
+  close[i].onclick = () => {
+    const div = this.parentElement;
+    div.style.opacity = '0';
+    setTimeout(() => { div.style.display = 'none'; }, 600);
+  };
 }
 
 
 function initMap() {
-    const locale = { lat: 6.5538235, lng: 3.3664734 };
-    const map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: locale });
-    const marker = new google.maps.Marker({
-        position: locale,
-        map
-    });
+  const locale = { lat: 6.5538235, lng: 3.3664734 };
+  const map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: locale });
+  const marker = new google.maps.Marker({
+    position: locale,
+    map
+  });
 
-    const infoWindow = new google.maps.InfoWindow({
-        content: `<p style="font-weight: bolder; color: #333;">Fast Food Fast Restaurant Building</p> <br> <p>Opens 8:30AM - 10:30PM everyday</p>`
-    });
-    marker.addListener('click', () => {
-        infoWindow.open(map, marker);
-    });
+  const infoWindow = new google.maps.InfoWindow({
+    content: '<p style="font-weight: bolder; color: #333;">Fast Food Fast Restaurant Building</p> <br> <p>Opens 8:30AM - 10:30PM everyday</p>'
+  });
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });
 }
+
+const showMenu = (elemID) => {
+  const menuItem = document.getElementById(elemID);
+  menuItem.classList.toggle('hide');
+};
+
+showMenu();
