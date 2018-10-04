d7487094fe2b0192676bf6202d094e066b56e83a
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2018-10-04 13:45:06 +0100

    [Finishes #160877538] Implement add new meal functionality

--- client/app/food.js
+++ client/app/food.js
@@ -54,3 +54,129 @@ const getAllMeals = async () => {
 
 // invoke getAllMeals function when the window load
 window.onload = getAllMeals();
+
+/**
+ * @description a function to validate new meal data input
+ * @param {String} title
+ * @param {String} description
+ * @param {String} price
+ * @returns {Object} an objects containing the error messages
+ */
+const validateMealInput = (title, description, price) => {
+  const errors = {};
+  if (title.trim() === '') {
+    errors.title = 'Food item title field can\'t be empty';
+  }
+  if (title.trim() !== '' && (title.length < 3 || title.length > 150)) {
+    errors.title = 'Food item title field should be within the range of 3 - 150 characters';
+  }
+  if (description.trim() === '') {
+    errors.description = 'Description field can\'t be empty';
+  }
+  if (description.trim() !== '' && (description.length < 3 || description.length > 300)) {
+    errors.description = 'Description field should be within the range of 3 - 300 characters';
+  }
+  if (Number.isNaN(price)) {
+    errors.price = 'Price of food item should be a number';
+  }
+  if (`${price}`[0] === '-' || price === 0) {
+    errors.price = 'Price of food item is invalid';
+  }
+  return errors;
+};
+
+
+  /**
+   * @description a function to handle the display of error messages
+   * @param {Object} errorsObject an Object containing errors
+   * @param {Object} divHolder a div tag that houses the error P tag
+   * @param {Object} pHolder a P tag that contains the error message
+   * @returns {undefined}
+   */
+const showErrorMessages = (errorsObject, divHolder, pHolder) => {
+  let err = '<b><i class="fa fa-warning"></i> Error!!!</b><br>';
+  Object.values(errorsObject).forEach((e) => {
+    err = `${err + e};<br>`;
+  });
+  pHolder.innerHTML = err;
+  divHolder.style.opacity = '1';
+  divHolder.style.display = 'block';
+  setTimeout(() => {
+    divHolder.style.display = 'none';
+  }, 5000);
+};
+
+/**
+ * @function
+ * @description a function to generate Image link
+ * @returns {String} imgUrl
+ */
+const genImgUrl = () => {
+  const randNum = Math.floor(Math.random() * Math.floor(6));
+  const imgUrlArrays = [
+    'https://res.cloudinary.com/faray/image/upload/v1538654131/f8.jpg',
+    'https://res.cloudinary.com/faray/image/upload/v1538654131/f10.jpg',
+    'https://res.cloudinary.com/faray/image/upload/v1538654130/f7.jpg',
+    'https://res.cloudinary.com/faray/image/upload/v1538654129/f6.jpg',
+    'https://res.cloudinary.com/faray/image/upload/v1538654128/f3.jpg',
+    'https://res.cloudinary.com/faray/image/upload/v1538654058/f4.jpg',
+  ];
+  const imgUrl = imgUrlArrays[randNum];
+  return imgUrl;
+};
+
+/**
+  * @function
+  * @description a funtion that handles the adding of new meal to menu
+  * @param {*} id
+  * @returns {undefined}
+  */
+// eslint-disable-next-line no-unused-vars
+const addMeal = async () => {
+  const errorDiv = document.getElementById('failAlert');
+  const errorMessage = document.getElementById('mealMsg');
+  const successDiv = document.getElementById('mealSuccessAlert');
+  const price = Number.parseInt(document.getElementById('price').value, 10);
+  const description = document.getElementById('desc').value;
+  const title = document.getElementById('title').value;
+  const mealData = JSON.stringify({
+    title,
+    price,
+    description,
+    imageUrl: genImgUrl()
+  });
+  const validationResult = validateMealInput(title, description, price);
+  if (Object.keys(validationResult).length > 0) {
+    showErrorMessages(validationResult, errorDiv, errorMessage);
+    return;
+  }
+
+  const url = '../api/v1/menu';
+  const resp = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`
+    }),
+    body: mealData
+  });
+
+  const result = await resp.json();
+  const {
+    status,
+    success,
+    meal,
+    error
+  } = result;
+  if (status === 201 && success && meal) {
+    successDiv.style.display = 'block';
+    setTimeout(() => {
+      successDiv.style.display = 'none';
+      window.location = 'foods.html';
+    }, 1000);
+    return;
+  } if (!success) {
+    showErrorMessages(error, errorDiv, errorMessage);
+  }
+};
